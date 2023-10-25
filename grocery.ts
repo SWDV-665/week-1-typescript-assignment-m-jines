d81980a63cb1a736bf9a262eb5c6ce81101e2347
Mary Jines <mbej1998@gmail.com>
2023-10-25 16:32:19 -0500

    .ts - .js

---
+++ grocery.ts
@@ -0,0 +1,27 @@
+class Grocery {
+    name: string;
+    price: number;
+    quantity: number;
+
+    constructor(name: string, price: number, quantity: number){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
+
+let groceryList = [
+    new Grocery("Eggs", 2.99, 2),
+    new Grocery("Milk", 3.50, 1),
+    new Grocery("Coffee", 4.99, 1),
+    new Grocery("Bananas", 0.99, 6)
+
+];
+
+const ele = document.getElementById("app");
+
+groceryList.forEach(e => {
+    const p = document.createElement("p");
+    p.textContent = `Item: ${e.name} Quantity: ${e.quantity} Price per Unit: $${e.price}`;
+    ele.appendChild(p);
+});
